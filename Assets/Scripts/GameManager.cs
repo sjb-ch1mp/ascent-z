0f88ee2da52eb837551d8942af3c4c1928a073f3
Samuel Brookes <samuel.brookes@anu.edu.au>
2024-09-28 23:14:14 +1000

    Initial UI : Weapons, Grenades, Armour, Health

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,29 +3,49 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject gameOverUI;
 
+    // Exports
+    public GameObject gameOverUI;
+    
+    // References
+    UserInterface ui;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        ui = GameObject.Find("UserInterface").GetComponent<UserInterface>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void PickUpWeapon(Resources.Weapon weapon) {
+        ui.PickUpWeapon(weapon);
     }
 
+    public void ConsumeAmmo() {
+        ui.ConsumeAmmo();
+    }
 
-    public void gameOver()
-    {
+    public void ConsumeGrenade() {
+        ui.ConsumeGrenade();
+    }
+
+    public bool HasAmmo() {
+        return ui.HasAmmo();
+    }
+
+    public bool HasGrenades() {
+        return ui.HasGrenades();
+    }
+
+    public void RenderLives(int lives) {
+        ui.RenderLives(lives);
+    }
+
+    public void gameOver() {
         gameOverUI.SetActive(true);
     }
 
-    public void restart()
-    {
+    public void restart() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
 }
